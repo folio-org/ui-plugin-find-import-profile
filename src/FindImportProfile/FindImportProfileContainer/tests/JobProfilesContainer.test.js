2ecaca07edcd3e3e85d3f37734fd4f8f7459c70c
Mariia Aloshyna <55138456+mariia-aloshyna@users.noreply.github.com>
2023-10-13 16:28:09 +0300

    Release v7.0.0 (#148)

--- src/FindImportProfile/FindImportProfileContainer/tests/JobProfilesContainer.test.js
+++ src/FindImportProfile/FindImportProfileContainer/tests/JobProfilesContainer.test.js
@@ -12,7 +12,7 @@ import {
   buildMutator,
   buildResources,
 } from '@folio/stripes-data-transfer-components/test/helpers';
-import { buildStripes } from '@folio/data-import/test/jest/helpers';
+import { buildStripes } from '../../../../test/jest/helpers';
 import { jobProfilesShape } from '@folio/data-import/src/settings/JobProfiles';
 import { translationsProperties } from '../../../../test/jest/helpers';
 

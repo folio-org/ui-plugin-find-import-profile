95d15dd4385fdc8a6d5cb4f8ecab73ca9bc87253
Oleksandr Hladchenko1 <oleksandr_hladchenko1@epam.com>
2024-01-05 15:15:46 +0200

    UIPFIMP-64: Cover fetchAssociations.js  with unit tests

---
+++ src/FindImportProfile/utils/fetchAssociations.test.js
@@ -0,0 +1,68 @@
+import faker from 'faker';
+
+import '../../../test/jest/__mock__';
+
+import { fetchAssociations } from './fetchAssociations';
+
+global.fetch = jest.fn();
+
+const mockData = { name: 'test name' };
+const profileId = faker.random.uuid();
+const okapi = {
+  url: 'https://test.com',
+  tenant: 'tenant',
+  token: 'token',
+};
+const path = `${okapi.url}/data-import-profiles/profileAssociations/${profileId}`;
+
+describe('fetchAssociations function', () => {
+  beforeEach(() => {
+    global.fetch.mockClear();
+  });
+
+  describe('when masterType is equal to parentType', () => {
+    it('should fetch details for the entity', async () => {
+      global.fetch.mockResolvedValueOnce({
+        ok: true,
+        status: 200,
+        json: async () => mockData,
+      });
+
+      const expectedUrl = `${path}/masters?detailType=JOB_PROFILE`;
+      const data = await fetchAssociations(okapi, profileId, 'ACTION_PROFILE', 'ACTION_PROFILE', 'jobProfiles');
+
+      expect(global.fetch.mock.calls[0][0]).toBe(expectedUrl);
+      expect(data).toEqual(mockData);
+    });
+  });
+
+  describe('when masterType is not equal to parentType', () => {
+    it('should fetch details for the master type', async () => {
+      global.fetch.mockResolvedValueOnce({
+        ok: true,
+        status: 200,
+        json: async () => mockData,
+      });
+
+      const expectedUrl = `${path}/details?masterType=ACTION_PROFILE`;
+      const data = await fetchAssociations(okapi, profileId, 'JOB_PROFILE', 'ACTION_PROFILE', 'actionProfiles');
+
+      expect(global.fetch.mock.calls[0][0]).toBe(expectedUrl);
+      expect(data).toEqual(mockData);
+    });
+  });
+
+  describe('when there is no data', () => {
+    it('should return an empty object', async () => {
+      global.fetch.mockResolvedValueOnce({
+        ok: false,
+        status: 400,
+        json: async () => null,
+      });
+
+      const data = await fetchAssociations(okapi, profileId, 'test', 'test', 'jobProfiles');
+
+      expect(data).toEqual({});
+    });
+  });
+});
